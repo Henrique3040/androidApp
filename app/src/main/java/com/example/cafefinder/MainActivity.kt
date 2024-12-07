ca3f05762d4e457da3a3869beebb351d66464467
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2024-12-07 20:04:16 +0100

    Front-end
    Layout voor the app gemaakt
    MainActivity gewiesld om met google places te werken
    
    reviewd by: Henrique

--- app/src/main/java/com/example/cafefinder/MainActivity.kt
+++ app/src/main/java/com/example/cafefinder/MainActivity.kt
@@ -1,47 +1,50 @@
 package com.example.cafefinder
-
+import android.app.Activity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cafefinder.ui.theme.CafeFinderTheme
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CafeFinderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+    private lateinit var selectLocation: LinearLayout
+    private lateinit var textSelectedLocation: TextView
+
+
+    val placesResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val intent = it.data
+                if (intent != null) {
+                    val place = Autocomplete.getPlaceFromIntent(intent)
+                    textSelectedLocation.text = place.address
+
                 }
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CafeFinderTheme {
-        Greeting("Android")
+        val mapsKey = BuildConfig.MAPS_KEY
+        if (!Places.isInitialized()) {
+            Places.initialize(this.applicationContext, mapsKey)
+        }
+
+        selectLocation = findViewById(R.id.select_location)
+        textSelectedLocation = findViewById(R.id.text_selected_location)
+
+        selectLocation.setOnClickListener {
+            val fields = listOf(Place.Field.ADDRESS)
+            val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(this)
+            placesResult.launch(intent)
+        }
     }
 }
